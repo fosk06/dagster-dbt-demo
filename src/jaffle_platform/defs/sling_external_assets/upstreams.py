2675222ba1480ec953db4ae4457ae8fc94e2deed
thomas trividic <thomas.trividic@external.thalesgroup.com>
2025-06-24 13:18:29 +0200

    monkey patch sling external assets

--- src/jaffle_platform/defs/sling_external_assets/upstreams.py
+++ src/jaffle_platform/defs/sling_external_assets/upstreams.py
@@ -11,9 +11,7 @@ def build_sling_raw_assets(replication_yaml_path):
         if stream_path.startswith("file://"):
             rel_path = stream_path.replace("file://", "")
             p = Path(rel_path)
-            # On construit la clé au format ["file_", "_jaffle_data_raw_xxx", "csv"]
-            # On prend le nom du fichier sans extension
-            stem = p.stem  # ex: raw_customers
+            stem = p.stem
             key = ["file_", f"_jaffle_data_{stem}", "csv"]
             assets.append(
                 dg.AssetSpec(key=key, group_name="ingestion")
