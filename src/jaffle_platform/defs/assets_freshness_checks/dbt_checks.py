a16103c2d3637d7dc84fa1da02207a9fb89bad62
thomas trividic <thomas.trividic@external.thalesgroup.com>
2025-06-24 19:29:56 +0200

    add freshness checks for datamarts

---
+++ src/jaffle_platform/defs/assets_freshness_checks/dbt_checks.py
@@ -0,0 +1,33 @@
+from datetime import timedelta
+import dagster as dg
+from dagster_dbt import dbt_assets
+
+# @dbt_assets(manifest="dbt/target/manifest.json")
+# def dbt_assets_fn(context, dbt):
+#     yield from dbt.cli(["build"], context=context).stream()
+
+# AssetKeys for all datamart assets
+asset_keys = [
+    dg.AssetKey(["target", "main", "tweets"]),
+    dg.AssetKey(["target", "main", "products"]),
+    dg.AssetKey(["target", "main", "orders"]),
+    dg.AssetKey(["target", "main", "order_items"]),
+    dg.AssetKey(["target", "main", "customers"]),
+    dg.AssetKey(["target", "main", "supplies"]),
+]
+
+freshness_checks = dg.build_last_update_freshness_checks(
+    assets=asset_keys,
+    lower_bound_delta=timedelta(minutes=3),
+    # deadline_cron="*/5 * * * *"
+)
+
+freshness_sensor = dg.build_sensor_for_freshness_checks(
+    freshness_checks=freshness_checks
+)
+
+# print(dbt_assets_fn)
+defs = dg.Definitions(
+    asset_checks=freshness_checks,
+    sensors=[freshness_sensor],
+)
