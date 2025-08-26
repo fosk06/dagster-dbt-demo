f1643dcf64761cda802751ed9780114db95eeeb9
thomas trividic <thomas.trividic@external.thalesgroup.com>
2025-08-26 20:58:22 +0200

    use same audits as dg-sqlmesm module

--- sqlmesh_project/models/stg/stg_stores.sql
+++ sqlmesh_project/models/stg/stg_stores.sql
@@ -3,10 +3,11 @@ MODEL (
   kind FULL,
   cron '*/5 * * * *',
   grain store_id,
-  tags ["dagster:group_name:staging", "staging"],
+  tags ["dagster:group_name:staging"],
   audits(
     number_of_rows(threshold := 5),
-    not_null(columns := (store_id))
+    not_null(columns := (store_id)),
+    not_constant(column := store_id)
   )
 );
 
